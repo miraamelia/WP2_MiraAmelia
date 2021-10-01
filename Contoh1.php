aede6145490f9d82a478b2fd4843d32d9cb9a06d
miraamelia <91109247+miraamelia@users.noreply.github.com>
2021-10-01 16:44:15 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Mira Amelia
+             Saya tinggal di daerah pegadungan jakarta barat
+             Hobi yang saya sukai adalah berolahraga";
+    }
+}
